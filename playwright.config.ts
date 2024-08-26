989a63f402ca136b9af2dadfe7ced725f4804b0e
Vitali Popov <123839092+Etelgast@users.noreply.github.com>
2024-08-26 14:44:06 +0300

    Update playwright.config.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -20,12 +20,12 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  // reporter: [['line'], ['allure-playwright', {
-  //   detail: true,
-  //   outputFolder: 'allure-results',
-  //   suiteTitle: true,
+  reporter: [['line'], ['allure-playwright', {
+    detail: true,
+    outputFolder: 'allure-results',
+    suiteTitle: true,
 
-  // }]],
+  }]],
   // reporter: [['line']],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
